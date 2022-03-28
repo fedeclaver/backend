2da437edefd5bee03ab8bb40f371faf6759edc8d
fedeclaver <fedeclaver@gmail.com>
2022-03-28 17:38:51 -0300

    fix compras product

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ module.exports = {
   },
   admin: true,
   mongodb: {
-    //cnxStr: process.env.MONGO_ATLAS,
-     cnxStr:'mongodb://localhost:27017/ecommerce',
+    cnxStr: process.env.MONGO_ATLAS,
+    // cnxStr:'mongodb://localhost:27017/ecommerce',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -21,18 +21,6 @@ module.exports = {
     admin: process.env.GMAIL_ADMIN,
   },
 
-  firebase: {
-    type: process.env.FIREBASE_type,
-    project_id: process.env.FIREBASE_project_id,
-    private_key_id: process.env.FIREBASE_private_key_id,
-    private_key: process.env.FIREBASE_client_email,
-    client_email: process.env.FIREBASE_private_key,
-    client_id: process.env.FIREBASE_client_id,
-    auth_uri: process.env.FIREBASE_auth_uri,
-    token_uri: process.env.FIREBASE_token_uri,
-    auth_provider_x509_cert_url: process.env.FIREBASE_auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.FIREBASE_client_x509_cert_url,
-  },
   // credenciales Twillio
   TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
